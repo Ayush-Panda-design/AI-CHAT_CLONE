a37d89ac01e500f077798b341bf98f87aa37df5d
Ayush <pandaayush25305@gmail.com>
2026-06-06 12:42:53 +0530

    Convert next.config to mjs to fix Render typescript missing module error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-import path from "path";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com", "res.cloudinary.com"],
   },
